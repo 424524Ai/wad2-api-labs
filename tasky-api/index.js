616fa3f6b08d8ae9d26c8ca5cd7948af64124d78
424524Ai <hleabrszylx@gmail.com>
2025-11-25 15:23:45 +0000

    Added Auth Middleware

--- tasky-api/index.js
+++ tasky-api/index.js
@@ -6,6 +6,7 @@ import './db';
 // other imports
 import cors from 'cors';
 import usersRouter from './api/users';
+import authenticate from './authenticate';
 
 dotenv.config();
 
@@ -27,7 +28,7 @@ const port = process.env.PORT;
 
 app.use(express.json());
 
-app.use('/api/tasks', tasksRouter);
+app.use('/api/tasks', authenticate, tasksRouter);
 
 app.use('/api/users', usersRouter);
 
